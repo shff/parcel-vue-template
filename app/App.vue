ae578b7e1a003a354613e9ae2b0c4c6a0e570330
Silvio Henrique Ferreira <shferreira@me.com>
2018-02-06 00:06:56 -0200

    Parcel

---
+++ app/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <img src="./logo.png">
+    <div class="hello">
+      <h1>Vue.js</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: "App",
+  };
+</script>
+
+<style lang="stylus" scoped>
+  main
+    font-family "Avenir", Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
+</style>
